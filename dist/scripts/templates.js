901cb6251c7a999873dfa48f66fda05972082580
badview08 <jonesgabriel21@gmail.com>
2015-07-31 20:51:13 -0500

    added simple header and footer and changed colors

--- dist/scripts/templates.js
+++ dist/scripts/templates.js
@@ -1,4 +1,4 @@
 this["JST"] = this["JST"] || {};
 this["JST"]["application"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    return "<h1>Hello</h1>\n";
+    return "<header>\n  <h1 class=\"header-text\">Cause I have to</h1>\n  <h2 class=\"header-text\">This website is dedicated to the tracking and record keeping of police involved shootings in the Contenintal United States</h2>\n</header>\n\n<footer>\n  <span class=\"footer-text\">Copyright (c) 2015 Copyright Holder All Rights Reserved.</span>\n</footer>\n";
 },"useData":true});
